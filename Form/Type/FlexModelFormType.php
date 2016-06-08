596edbcb430dcdddc66f959815b30734a79e9b84
Nick van der Zwet <nick@connectholland.nl>
2016-06-08 12:22:35 +0200

    added birthdaytype namespace to fix #38 (#39)
    Add BirthdayType namespace to fix #38

--- Form/Type/FlexModelFormType.php
+++ Form/Type/FlexModelFormType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
